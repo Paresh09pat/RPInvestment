4d8661d7cd5574c1c4027e5ec1904e5c36145fee
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2023-12-18 17:01:56 +0530

    pass

--- client/src/Components/REGISTRATION/LoginPass/LoginPass.jsx
+++ client/src/Components/REGISTRATION/LoginPass/LoginPass.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
 import eyeOutline from '@iconify/icons-mdi/eye-outline';
 import eyeOffOutline from '@iconify/icons-mdi/eye-off-outline';
