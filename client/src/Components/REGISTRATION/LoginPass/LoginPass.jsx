642645b67e0f18d4c8256b9e2a61b2a7dae1aa7c
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2023-12-18 16:56:26 +0530

    strengh bar

--- client/src/Components/REGISTRATION/LoginPass/LoginPass.jsx
+++ client/src/Components/REGISTRATION/LoginPass/LoginPass.jsx
@@ -1,101 +1,178 @@
-import React, { useState } from 'react';
-import { Icon } from '@iconify/react';
-import eyeOutline from '@iconify/icons-mdi/eye-outline';
-import eyeOffOutline from '@iconify/icons-mdi/eye-off-outline';
-import infoIcon from '@iconify-icons/bi/info';
-import "./LoginPass.css"
+import React, { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
+import eyeOutline from "@iconify/icons-mdi/eye-outline";
+import eyeOffOutline from "@iconify/icons-mdi/eye-off-outline";
+import infoIcon from "@iconify-icons/bi/info";
+import "./LoginPass.css";
 
 function LoginPass() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [requirementsMet, setRequirementsMet] = useState({
+    length: false,
+    upperLowerNum: false,
+    specialChars: false,
+  });
 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const togglePasswordVisibility = (field) => {
+    if (field === "password") {
+      setShowPassword(!showPassword);
+    } else if (field === "confirmPassword") {
+      setShowConfirmPassword(!showConfirmPassword);
+    }
+  };
 
+  useEffect(() => {
+    // Check password requirements and update state
+    const checkRequirements = () => {
+      const lengthRequirement = password.length >= 8;
+      const upperLowerNumRequirement =
+        /[A-Z]/.test(password) && /[a-z]/.test(password) && /\d/.test(password);
+      const specialCharsRequirement = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-
-    const togglePasswordVisibility = (field) => {
-        if (field === 'password') {
-            setShowPassword(!showPassword);
-        } else if (field === 'confirmPassword') {
-            setShowConfirmPassword(!showConfirmPassword);
-        }
+      setRequirementsMet({
+        length: lengthRequirement,
+        upperLowerNum: upperLowerNumRequirement,
+        specialChars: specialCharsRequirement,
+      });
     };
 
-    return (
-        <>
-            <div className='Loginpass_main_div'>
-                <div id='Main_container_div_LP'>
-                    <div className="Head_main_loginPass">
-                        Login Password Setup
-                    </div>
+    checkRequirements();
+  }, [password]);
 
-                    <div className='whole_pass_main_container'>
-                        <div className="password-container">
-                            <input
-                                type={showPassword ? 'text' : 'password'}
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Enter Password"
-                                className='input_LPS input-text newani'
-                            />
-                            <span
-                                className="toggle-eye-btn"
-                                onClick={() => togglePasswordVisibility('password')}
-                            >
-                                <Icon icon={showPassword ? eyeOutline : eyeOffOutline} />
-                            </span>
-                        </div>
+  // Calculate the strength bar width and color
+  const calculateStrengthBar = () => {
+    const checkedCount = Object.values(requirementsMet).filter(Boolean).length;
+    switch (checkedCount) {
+      case 3:
+        return { width: "100%", background: "green" };
+      case 2:
+        return { width: "60%", background: "orange" };
+      case 1:
+        return { width: "20%", background: "red" };
+      default:
+        return { width: "0%", background: "transparent" };
+    }
+  };
 
-                        <div className='All_checkbox_comdiv'>
-                            <ul className="pass_set_check">
-                                <li className="arrange">
-                                    <input type="checkbox" id="showPasswordLength" disabled />
-                                    <label for="showPassword" className='label-info'> Let's start with at least 8 characters. The longer, the better!.</label>
-                                </li>
-                                <li className="arrange">
-                                    <input type="checkbox" id="showPasswordUpperLowerNum" disabled />
-                                    <label for="showPassword" className='label-info'> Mix it up! A combo of at least 1 uppercase letter, 1 lowercase letter, and number.</label>
-                                </li>
-                                <li className="">
-                                    <input type="checkbox" id="showPasswordSpecialChars" disabled />
-                                    <label for="showPassword" className='label-info'> Throw in a special character like @, #, $, or % to make it extra secure!</label>
-                                </li>
-                            </ul>
-                        </div>
+  const strengthBarStyle = calculateStrengthBar();
 
-                        <div className="password-container">
-                            <input
-                                type={showConfirmPassword ? 'text' : 'password'}
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                placeholder="Confirm Password"
-                                className='input_LPS input-text newani'
-                            />
-                            <span
-                                className="toggle-eye-btn"
-                                onClick={() => togglePasswordVisibility('confirmPassword')}
-                            >
-                                <Icon icon={showConfirmPassword ? eyeOutline : eyeOffOutline} />
-                            </span>
-                        </div>
+  return (
+    <>
+      <div className="Loginpass_main_div">
+        <div id="Main_container_div_LP">
+          <div className="Head_main_loginPass">Login Password Setup</div>
 
-                        <div className='btn_tip_comdiv'>
-                            <button className='form-button button-fancy -black' >
-                                <span className="arrow"></span>
-                                <span className="text">Submit</span>
-                            </button>
-
-                            <p className="circle-icon1" >
-                                <Icon icon={infoIcon} style={{ color: 'black' }} width="40" height="40" />
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
+          <div className="whole_pass_main_container">
+            <div className="password-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Password"
+                className="input_LPS input-text newani"
+              />
+              <span
+                className="toggle-eye-btn"
+                onClick={() => togglePasswordVisibility("password")}
+              >
+                <Icon icon={showPassword ? eyeOutline : eyeOffOutline} />
+              </span>
+              <div className="strength-container" >
+                <div
+                  className="strength-bar"
+                  style={{
+                    width: `${strengthBarStyle.width}`,
+                    backgroundColor: `${strengthBarStyle.background}`,
+                    height: "5px",
+                    borderRadius: "2px",
+                  }}
+                ></div>
+              </div>
             </div>
-        </>
-    )
+
+            <div className="All_checkbox_comdiv">
+              <ul className="pass_set_check">
+                <li className="arrange">
+                  <input
+                    type="checkbox"
+                    id="showPasswordLength"
+                    checked={requirementsMet.length}
+                    disabled
+                  />
+                  <label htmlFor="showPassword" className="label-info">
+                    {" "}
+                    Let&apos;s start with at least 8 characters. The longer, the
+                    better!.
+                  </label>
+                </li>
+                <li className="arrange">
+                  <input
+                    type="checkbox"
+                    id="showPasswordUpperLowerNum"
+                    checked={requirementsMet.upperLowerNum}
+                    disabled
+                  />
+                  <label htmlFor="showPassword" className="label-info">
+                    {" "}
+                    Mix it up! A combo of at least 1 uppercase letter, 1
+                    lowercase letter, and number.
+                  </label>
+                </li>
+                <li className="">
+                  <input
+                    type="checkbox"
+                    id="showPasswordSpecialChars"
+                    checked={requirementsMet.specialChars}
+                    disabled
+                  />
+                  <label htmlFor="showPassword" className="label-info">
+                    {" "}
+                    Throw in a special character like @, #, $, or % to make it
+                    extra secure!
+                  </label>
+                </li>
+              </ul>
+            </div>
+
+            <div className="password-container">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm Password"
+                className="input_LPS input-text newani"
+              />
+              <span
+                className="toggle-eye-btn"
+                onClick={() => togglePasswordVisibility("confirmPassword")}
+              >
+                <Icon icon={showConfirmPassword ? eyeOutline : eyeOffOutline} />
+              </span>
+            </div>
+
+            <div className="btn_tip_comdiv">
+              <button className="form-button button-fancy -black">
+                <span className="arrow"></span>
+                <span className="text">Submit</span>
+              </button>
+
+              <p className="circle-icon1">
+                <Icon
+                  icon={infoIcon}
+                  style={{ color: "black" }}
+                  width="40"
+                  height="40"
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default LoginPass
+export default LoginPass;
