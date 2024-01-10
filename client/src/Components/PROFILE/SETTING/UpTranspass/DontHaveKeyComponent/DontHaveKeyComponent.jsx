5c6b3f8a1501b38e03f358a59bfa4d0b260e2d5e
Paresh <09patilparesh@gmail.com>
2024-01-10 14:04:35 +0530

    today

--- client/src/Components/PROFILE/SETTING/UpTranspass/DontHaveKeyComponent/DontHaveKeyComponent.jsx
+++ client/src/Components/PROFILE/SETTING/UpTranspass/DontHaveKeyComponent/DontHaveKeyComponent.jsx
@@ -20,6 +20,10 @@ function DontHaveKeyComponent() {
         <h2 className='DN_Adjust_head'>Contact To Support</h2>
         <h3 className='DN_Adjust_head'>Lost Your 12-Phrase Key? Don't Worry, We're Here to Help!</h3>
 
+        <p className='in_text_added'>
+        We understand that things happen, and that's why we've got a robust system in place to help you recover your account or retrieve your 12-phrase key. Follow the steps below to get the assistance you need.
+        </p>
+
         <div className='DN_NUMMail_main'>
           <div className='DN_NUMMail_sub'>
             <div className='sub_div_1'>Write an Email to :</div>
